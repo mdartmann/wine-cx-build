665e0ee235bd4147ed31a14da47543f9cc5a4c76
Mae Dartmann <hello@maedartmann.name>
2025-09-29 15:30:37 +0200

    tweak build script: remove `alias`, shellcheck, echo out steps

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -ue
 
-export GITHUB_WORKSPACE="$(pwd)"
+export GITHUB_WORKSPACE="${PWD}"
 export CROSS_OVER_VERSION="25.1.0"
 export CROSS_OVER_SOURCE_URL=https://media.codeweavers.com/pub/crossover/source/crossover-sources-${CROSS_OVER_VERSION}.tar.gz
 export CROSS_OVER_LOCAL_FILE=crossover-${CROSS_OVER_VERSION}
@@ -10,11 +10,10 @@ export INSTALLROOT="${GITHUB_WORKSPACE}"/install
 export WINE_INSTALLATION=wine-cx${CROSS_OVER_VERSION}
 
 eval "$(/usr/local/bin/brew shellenv)"
-alias brew="arch -x86_64 brew"
 export PATH="/usr/local/opt/llvm/bin:$PATH"
 export PATH="/usr/local/opt/bison/bin:$PATH"
 export PATH="/usr/local/opt/libpcap/bin:$PATH"
-export PKG_CONFIG_PATH="/usr/local/opt/libpcap/lib/pkgconfig:/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="/usr/local/opt/libpcap/lib/pkgconfig:/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 export CC="clang -arch x86_64"
 export CXX="clang++ -arch x86_64"
 export i386_CC="i686-w64-mingw32-gcc"
@@ -28,21 +27,27 @@ export CROSSCFLAGS="${OPTFLAGS} -Wno-incompatible-pointer-types"
 export CPPFLAGS="-I/usr/local/opt/llvm/include -I/usr/local/opt/bison/include"
 export LDFLAGS="-L/usr/local/opt/llvm/lib -L/usr/local/opt/bison/lib -Wl,-headerpad_max_install_names -Wl,-rpath,@loader_path/../../ -Wl,-rpath,/usr/local/lib -Wl,-rpath,/opt/X11/lib"
 export ac_cv_lib_soname_vulkan=""
-export DYLD_FALLBACK_LIBRARY_PATH="${DYLD_FALLBACK_LIBRARY_PATH}:/usr/lib:/usr/X11/lib"
+export DYLD_FALLBACK_LIBRARY_PATH="${DYLD_FALLBACK_LIBRARY_PATH:-}:/usr/lib:/usr/X11/lib"
 
 if [[ ! -f ${CROSS_OVER_LOCAL_FILE}.tar.gz ]]; then
+    echo -e "\033[1;32m→ downloading crossover sources...\033[0m"
     curl -o ${CROSS_OVER_LOCAL_FILE}.tar.gz ${CROSS_OVER_SOURCE_URL}
     if [[ -d "${GITHUB_WORKSPACE}/sources" ]]; then
         rm -rfv "${GITHUB_WORKSPACE}"/sources
     fi
-    tar xvf ${CROSS_OVER_LOCAL_FILE}.tar.gz
+    echo -e "\033[1;32m→ extracting crossover sources...\033[0m"
+    tar xf ${CROSS_OVER_LOCAL_FILE}.tar.gz
 fi
 
 cp "${GITHUB_WORKSPACE}"/distversion.h "${GITHUB_WORKSPACE}"/sources/wine/programs/winedbg/distversion.h
 
-brew update
-brew upgrade
-brew install bison mingw-w64 llvm lld gettext gstreamer pkgconfig freetype gnutls molten-vk sdl2
+echo -e "\033[1;32m→ installing x86_64 dependencies through homebrew...\033[0m"
+
+arch -x86_64 brew update
+arch -x86_64 brew upgrade
+arch -x86_64 brew install bison mingw-w64 llvm lld gettext gstreamer pkgconfig freetype gnutls molten-vk sdl2
+
+echo -e "\033[1;32m→ configuring wine build...\033[0m"
 
 mkdir -p "${BUILDROOT}"/winecx-${CROSS_OVER_VERSION}
 pushd "${BUILDROOT}"/winecx-${CROSS_OVER_VERSION}
@@ -85,9 +90,13 @@ ${WINE_CONFIGURE} \
     --without-x
 popd
 
+echo -e "\033[1;32m→ building wine, this may take a while...\033[0m"
+
 pushd "${BUILDROOT}"/winecx-${CROSS_OVER_VERSION}
-make -j$(sysctl -n hw.ncpu 2>/dev/null)
+make -j"$(sysctl -n hw.ncpu 2>/dev/null)"
 popd
+
+echo -e "\033[1;32m→ installing wine under ${BUILDROOT}...\033[0m"
 
 pushd "${BUILDROOT}"/winecx-${CROSS_OVER_VERSION}
 make install-image DESTDIR="${INSTALLROOT}/${WINE_INSTALLATION}"
